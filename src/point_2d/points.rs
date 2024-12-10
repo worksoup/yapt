2506a09cb07564e34d69a1e60e73073845025fdc
汤意工坊 <78455197+worksoup@users.noreply.github.com>
2024-12-10 16:00:21 +0800

    Init.

--- src/point_2d/points.rs
+++ src/point_2d/points.rs
@@ -19,11 +19,47 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-
+#![allow(unused_macros)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
 use crate::{Point, Point2D};
+
+trait Construct<T> {
+    fn new(point_2d: Point<T>) -> Self;
+}
+
+macro_rules! impl_construct_new {
+    ($root:ident $(:: $idents:ident )*<$t:ident$(,$other:ident)*>$(,$modifiers:tt)?) => {
+        impl<$t$(,$other)*> Construct<$t> for $root$(::$idents)*<$t$(,$other)*> {
+            fn new(point_2d: Point<$t>) -> Self {
+                Self::new($($modifiers)? point_2d.x,$($modifiers)? point_2d.y)
+            }
+        }
+    };
+}
+macro_rules! impl_construct_tuple {
+    ($root:ident $(:: $idents:ident )*<$t:ident$(,$other:ident)*>$(,$modifiers:tt)?) => {
+        impl<$t$(,$other)*> Construct<$t> for $root$(::$idents)*<$t$(,$other)*> {
+            fn new(point_2d: Point<$t>) -> Self {
+                Self($($modifiers)? point_2d.x,$($modifiers)? point_2d.y)
+            }
+        }
+    };
+}
+
+macro_rules! impl_construct_struct {
+    ($root:ident $(:: $idents:ident )*<$t:ident$(,$other:ident)*>$(,$modifiers:tt)?) => {
+        impl<$t$(,$other)*> Construct<$t> for $root$(::$idents)*<$t$(,$other)*> {
+            fn new(point_2d: Point<$t>) -> Self {
+                Self{x:$($modifiers)? point_2d.x,y:$($modifiers)? point_2d.y}
+            }
+        }
+    };
+}
+
 macro_rules! impl_point2d {
-    ($root:ident $(:: $idents:ident )*<$t:ident>) => {
-        impl<$t> Point2D<$t> for $root$(::$idents)*<$t> {
+    ($root:ident $(:: $idents:ident )*<$t:ident$(,$other:ident)*>) => {
+        impl<$t$(,$other)*> Point2D<$t> for $root$(::$idents)*<$t$(,$other)*> {
             fn rx(&self) -> &$t {
                 &self.x
             }
@@ -40,10 +76,7 @@ macro_rules! impl_point2d {
             }
 
             fn from_point_2d(point_2d: Point<$t>) -> Self {
-                Self {
-                    x: point_2d.x,
-                    y: point_2d.y,
-                }
+                <Self as Construct<T>>::new(point_2d)
             }
         }
     };
@@ -51,5 +84,15 @@ macro_rules! impl_point2d {
 
 #[cfg(feature = "rxing")]
 impl_point2d!(rxing::PointT<T>);
+#[cfg(feature = "rxing")]
+impl_construct_struct!(rxing::PointT<T>);
+
 #[cfg(feature = "imageproc")]
 impl_point2d!(imageproc::point::Point<T>);
+#[cfg(feature = "imageproc")]
+impl_construct_struct!(imageproc::point::Point<T>);
+
+#[cfg(feature = "euclid")]
+impl_point2d!(euclid::Point2D<T, U>);
+#[cfg(feature = "euclid")]
+impl_construct_new!(euclid::Point2D<T, U>);
